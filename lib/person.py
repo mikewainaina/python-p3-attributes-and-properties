27806abc4cdac4d996004d6e2d839fa03e145094
Mike <162094676+mikewainaina@users.noreply.github.com>
2024-05-27 09:26:31 +0300

    Update person.py

--- lib/person.py
+++ lib/person.py
@@ -1,20 +1,4 @@
 #!/usr/bin/env python3
-
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
